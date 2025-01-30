4bba23a93548c67bd91df31113dd93e317897ac3
LesGo-QueueEase <lesgoqueueease@gmail.com>
2025-01-30 10:53:30 +0800

    Delete src directory

--- src/data/user.js
+++
@@ -1,8 +0,0 @@
-const credentials = {
-  email: "test@gmail.com",
-  password: "Test@12345",
-  displayName: "Kyle Hervey M. Espiritu",
-  userType: "registrar", 
-};
-
-export default credentials;
--- src/pages/Errorpage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Errorpage = () => {
-  return <div className="w-full justify-center items-center">Errorpage</div>;
-};
-
-export default Errorpage;
